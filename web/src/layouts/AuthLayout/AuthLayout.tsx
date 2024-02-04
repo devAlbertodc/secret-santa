f096bba7d498797b58ac5c0a7b7065f28d2f95f6
devAlbertodc <aldacala@hotmail.es>
2024-02-04 20:27:16 +0100

    Change space between logo and forms below

--- web/src/layouts/AuthLayout/AuthLayout.tsx
+++ web/src/layouts/AuthLayout/AuthLayout.tsx
@@ -1,4 +1,4 @@
-import Footer from "src/components/Footer/Footer"
+import Footer from 'src/components/Footer/Footer'
 
 type AuthLayoutProps = {
   children?: React.ReactNode
@@ -7,15 +7,22 @@ type AuthLayoutProps = {
 const AuthLayout = ({ children }: AuthLayoutProps) => {
   //The background needs a height and width, because by default is 0, so it would be blank.
   //The background will be of 1440 but if it is wider, then will be 100% of the screen
-  return <>
-    <main
-      className="min-h-screen min-w-screen bg-auth bg-silverTree dark:bg-nileBlue bg-no-repeat 
-              bg-[center_top] bg-[length:1440px_auto] min-[1440px]:bg-[length:100%_auto]">
-      <img src="/images/logo__secret-santa.svg" alt="Secret Santa" className="mx-auto mb-24 w-[460px] pt-24"/>
-      {children}
-    </main>
-    <Footer />
-  </>
+  return (
+    <>
+      <main
+        className="min-w-screen min-h-screen bg-silverTree bg-auth bg-[length:1440px_auto] bg-[center_top]
+              bg-no-repeat dark:bg-nileBlue min-[1440px]:bg-[length:100%_auto]"
+      >
+        <img
+          src="/images/logo__secret-santa.svg"
+          alt="Secret Santa"
+          className="mx-auto mb-10 w-[460px] pt-24"
+        />
+        {children}
+      </main>
+      <Footer />
+    </>
+  )
 }
 
 export default AuthLayout
